7ce783d6a3516cf462e08bdd919a16645c36fc37
Ali Aryani <lenesis@proton.me>
2025-10-26 01:33:16 +0330

    get sys choice

--- src/main.zig
+++ src/main.zig
@@ -2,5 +2,8 @@ const std = @import("std");
 var stdout_buffer:[1024]u8 = undefined;
 var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
 const stdout = &stdout_writer.interface;
+const api = @import("api.zig");
 
-
+pub fn main() !void {
+    const sys_choice = api.get_sys_choice();
+}
