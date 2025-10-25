d5111ea4d52f69542c3ef167eced9240b50a3e26
Ali Aryani <lenesis@proton.me>
2025-10-26 01:30:01 +0330

    rewrite random syntax based on zig 0.15.2

--- src/api.zig
+++ src/api.zig
@@ -1,11 +1,10 @@
 const std = @import("std");
-var prng = std.Random.DefaultPrng.init(blk: {
-    var seed:u64 = undefined;
-    std.posix.getrandom(std.mem.asBytes(&seed));
-    break :blk seed;
-});
-const rand = prng.random();
 
 pub fn get_sys_choice() u8 {
-    return rand.int(u8);
+    const timestamp:u64 = @intCast(std.time.nanoTimestamp() & 0xFFFF_FFFF_FFFF_FFFF);
+    var prng = std.Random.DefaultPrng.init(timestamp);
+    const random = prng.random();
+    const choice = random.intRangeLessThan(u8, 1, 4);
+    std.debug.print("sys choice: {d}\n", .{choice});
+    return choice;
 }
