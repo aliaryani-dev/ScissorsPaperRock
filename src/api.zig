af9c2736c2db742154e50d47d64f796022b93e0d
Ali Aryani <lenesis@proton.me>
2025-10-26 01:36:41 +0330

    import stdin and stdout

--- src/api.zig
+++ src/api.zig
@@ -1,4 +1,10 @@
 const std = @import("std");
+var stdout_buffer:[1024]u8 = undefined;
+var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+const stdout = &stdout_writer.interface;
+var stdin_buffer:[1024]u8 = undefined;
+var stdin_writer = std.fs.File.stdin().reader(&stdin_buffer);
+const stdin = &stdin_writer.interface;
 
 pub fn get_sys_choice() u8 {
     const timestamp:u64 = @intCast(std.time.nanoTimestamp() & 0xFFFF_FFFF_FFFF_FFFF);
@@ -7,4 +13,7 @@ pub fn get_sys_choice() u8 {
     const choice = random.intRangeLessThan(u8, 1, 4);
     std.debug.print("sys choice: {d}\n", .{choice});
     return choice;
+}
+
+pub fn get_user_choice() u8 {
 }
